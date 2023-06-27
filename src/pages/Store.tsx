d5c0feef744894af347528b2d9c4da85562fdddc
Tanya <taniyhamail@gmail.ua>
2023-06-27 11:51:49 +0300

    feat: added 'store Page'

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,19 @@
+import { Col, Row } from 'react-bootstrap'
+
+import storeItemsList from '../data/items.json'
+import { StoreItem } from '../components/StoreItem'
+
 export function Store() {
-  return <h1>Store</h1>
+  return (
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItemsList.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
 }
